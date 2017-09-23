659e583a129d1ccbb3b9ed9d56fdb05cdf827d00
chandruark <kchandruk7@gmail.com>
2017-09-23 10:13:07 +0530

    all working as concerned for version 1.0.0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+ //This is a manifest file that'll be compiled into application.js, which will include all the files
+//listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
@@ -9,8 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,14 @@ class CommentsController < ApplicationController
 
 	end
 
+	def show
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+
+	end
+
 	private def comment_params
 		params.require(:comment).permit(:username, :body)
 	end
